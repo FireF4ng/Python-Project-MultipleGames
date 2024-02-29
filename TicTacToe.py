c21fb313ad60ff34310dc0de79c9033e30b40e28
FireFang <118684614+FireF4ng@users.noreply.github.com>
2024-02-29 11:50:21 +0100

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,103 @@
+import numpy as np
+import random
+import tkinter as tk
+from tkinter import messagebox
+from main import Main_menu
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self.game = tk.Tk()
+        self.board = [[None for _ in range(3)] for _ in range(3)]
+        self.player = ''
+        self.coin_toss()
+        self.ui()
+
+    def coin_toss(self):
+        """Function that determines what player goes first as X and what player gets 0"""
+        tmp = random.randint(1, 2)
+        if tmp == 1:
+            self.player = 'X'
+        else:
+            self.player = 'O'
+
+    def ui(self):
+        """Function that creates the board and UI"""
+        width = 600
+        height = 600
+        screenwidth = self.game.winfo_screenwidth()
+        screenheight = self.game.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.game.geometry(alignstr)
+        self.game.resizable(width=False, height=False)
+
+        for row in range(3):
+            self.game.rowconfigure(row, weight=1, minsize=50)
+            self.game.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3):
+                self.board[row][col] = tk.Button(self.game,
+                                                 justify="center",
+                                                 text=" ",
+                                                 width=30,
+                                                 height=30,
+                                                 command=lambda x=row, y=col: self.button(x, y))
+                self.board[row][col].grid(row=row, column=col)
+
+    def button(self, x, y):
+        print(self.board[x][y]['text'])
+        if self.board[x][y]['text'] == " ":
+            self.board[x][y]['text'] = self.player
+            if self.check_winner():
+                messagebox.showinfo("Tic Tac Toe ", "Player {} wins!".format(self.player))
+                self.game.destroy()
+                Main_menu()
+            elif self.check_draw():
+                messagebox.showinfo("Tic Tac Toe", "It's a draw!")
+                self.game.destroy()
+                Main_menu()
+            else:
+                self.next_player()
+
+    def check_winner(self):
+        """Function that checks the board for a winner. It checks colons, rows and diagonals and returns False if
+        there are still moves to make and no winners, True if there is a winner"""
+        # Check Rows
+        for row in self.board:
+            if row[0]['text'] == row[1]['text'] == row[2]['text'] != " ":
+                return True
+
+        # Check Cols
+        for col in range(3):
+            if self.board[0][col]['text'] == self.board[1][col]['text'] == self.board[2][col]['text'] != " ":
+                return True
+
+        # Check Diagonals
+        if (
+                self.board[0][0]['text'] == self.board[1][1]['text'] == self.board[2][2]['text'] != " "
+                or self.board[0][2]['text'] == self.board[1][1]['text'] == self.board[2][0]['text'] != " "
+        ):
+            return True
+
+        # No Winners
+        return False
+
+    def check_draw(self):
+        """Function that checks for a draw"""
+        for row in self.board:
+            for btn in row:
+                if " " in btn['text']:
+                    return False
+        return True
+
+    def check_move(self, x, y):
+        """Function that checks if the move is possible, it returns True if it's possible and False if it's not"""
+        if self.board[x][y] == 0:
+            return True
+        return False
+
+    def next_player(self):
+        if self.player == 'X':
+            self.player = 'O'
+        else:
+            self.player = 'X'
--- main.py
+++ main.py
@@ -1,116 +1,92 @@
-import numpy as np
-import random
-import tkinter as tk
+from TicTacToe import *
+from tkinter import *
 
-class TicTacToeGame:
+
+class Main_menu:
     def __init__(self):
-        self.window = tk.Tk()
-        self._create_ui_menu()
-        self.board = np.zeros((3, 3))
-        self.player = 1
-        self.state = 0
-        self._cells = {}
+        self.window = None
+        self.create()
+        self.menu()
 
-    def turn(self):
-        """Main Function of the games that works in turns"""
-        self.player = self.coin_toss()
-        while self.state == 0:
-            x = int(input("Chose the Row you want to play: ")) - 1
-            y = int(input("Chose the Column you want to play: ")) - 1
-            tmp = True
-            while not (tmp == False):
-                if self.check_move(x, y):
-                    self.board[x][y] = self.player
-                    tmp = False
-                    print(self.board)
-                else:
-                    print("Impossible because case already used")
-                    print(self.board)
-                    print("Re-chose the placement")
-                    x = int(input("Chose the Row you want to play: ")) - 1
-                    y = int(input("Chose the Column you want to play: ")) - 1
-            self.state = self.check_winner()
-            self.next_player()
-        if self.state == 1:
-            print("Player 1 wins")
-        elif self.state == 2:
-            print("Player 2 wins")
-        else:
-            print("Tie")
+    def tic_tac_toe(self):
+        self.window.destroy()
+        self.create()
+        self.window.title("Tic Tac Toe dificulty selector")
+        self.window.geometry("500x500")
+        tictactoe_B1 = Button(self.window, text="Easy", fg='deep sky blue', width=500, font='summer', bd=5,
+                              command=lambda: (self.window.destroy(), TicTacToe()))
+        tictactoe_B2 = Button(self.window, text="Medium", fg='deep sky blue', width=500, font='summer', bd=5,
+                              command=lambda: (self.window.destroy(), TicTacToe()))
+        tictactoe_B3 = Button(self.window, text="Hard", fg='deep sky blue', width=500, font='summer', bd=5,
+                              command=lambda: (self.window.destroy(), TicTacToe()))
+        tictactoe_B4 = Button(self.window, text="Back", fg='deep sky blue', width=500, pady=15, font='summer',
+                              bd=5,
+                              command=lambda: (self.window.destroy(), Main_menu()))
 
-    def next_player(self):
-        if self.player == 1:
-            self.player = 2
-        else:
-            self.player = 1
-
-    def coin_toss(self):
-        """Function that determines what player goes first as X and what player gets 0"""
-        return random.randint(1, 2)
-
-    def check_winner(self):
-        """Function that checks the board for a winner. It checks colons, rows and diagonals and returns 0 if there are still
-        moves to make and no winners, 1 if player 1 wins, 2 if player 2 wins and 3 if it is a tie"""
-        # Check Rows
-        for i in range(np.shape(self.board)[0]):
-            if self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2] and np.all(
-                    self.board != 0):
-                return self.board[i][0]
-
-        # Check Cols
-        for i in range(np.shape(self.board)[1]):
-            if self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i] and np.all(
-                    self.board != 0):
-                return self.board[0][i]
-
-        # Check Diagonals
-        if np.all(self.board.diagonal() == 1) or np.all(self.board.diagonal() == 2):
-            return self.board[0][0]
-        elif np.all(np.fliplr(self.board).diagonal() == 1) or np.all(np.fliplr(self.board).diagonal() == 2):
-            return self.board[0][-1]
-
-        # Check Tie
-        if np.all(self.board != 0):
-            return 3
-        return 0
-
-    def check_move(self, x, y):
-        """Function that checks if the move is possible, it returns True if it's possible and False if it's not"""
-        if self.board[x][y] == 0:
-            return True
-        return False
-
-    def _create_ui_menu(self):
-        width = 600
-        height = 500
-        screenwidth = self.window.winfo_screenwidth()
-        screenheight = self.window.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        self.window.geometry(alignstr)
-        self.window.resizable(width=False, height=False)
-
-        start_button = tk.Button(self.window)
-        start_button["justify"] = "center"
-        start_button["text"] = "Start"
-        start_button.place(x=160, y=100, width=303, height=75)
-        start_button["command"] = self.start_button
-
-        difficulty_button = tk.Button(self.window)
-        difficulty_button["justify"] = "center"
-        difficulty_button["text"] = "Leave"
-        difficulty_button.place(x=160, y=280, width=303, height=75)
-        difficulty_button["command"] = self.difficulty_button()
-
-        leave_button = tk.Button(self.window)
-        leave_button["justify"] = "center"
-        leave_button["text"] = "Difficulty: Easy"
-        leave_button.place(x=160, y=190, width=303, height=75)
-        leave_button["command"] = self.leave_button()
+        tictactoe_B1.pack(side='top', pady=15)
+        tictactoe_B2.pack(side='top', pady=15)
+        tictactoe_B3.pack(side='top', pady=15)
+        tictactoe_B4.pack(side='top', pady=15)
         self.window.mainloop()
 
-    def start_button(self):
-        grid_frame = tk.Frame(master=self.window)
-        grid_frame.pack()
+    def connect4(self, solo_choise):
+        pass
+
+    def solo(self):
+        self.window.destroy()
+        self.create()
+        self.window.title("Solo game selector")
+        self.window.geometry("500x500")
+        solo_B1 = Button(self.window, text="Tic Tac Toe", fg='deep sky blue', width=500, font='summer', bd=5,
+                         command=lambda: self.tic_tac_toe())
+        solo_B2 = Button(self.window, text="Connect 4", fg='deep sky blue', width=500, font='summer', bd=5,
+                         command=lambda: self.connect4())
+        solo_B3 = Button(self.window, text="Back", fg='deep sky blue', width=500, font='summer', bd=5,
+                         command=lambda: (self.window.destroy(), Main_menu()))
+        solo_B1.pack(side='top', pady=15)
+        solo_B2.pack(side='top', pady=15)
+        solo_B3.pack(side='top', pady=15)
+        self.window.mainloop()
+
+    def multiplayer(self):
+        self.window.destroy()
+        self.create()
+        self.window.title("Multiplayer game selector")
+        self.window.geometry("500x500")
+        multiplayer_B1 = Button(self.window, text="Tic Tac Toe", fg='deep sky blue', width=500, font='summer',
+                                bd=5,
+                                command=lambda: self.tic_tac_toe())
+        multiplayer_B2 = Button(self.window, text="Connect 4", fg='deep sky blue', width=500, font='summer',
+                                bd=5,
+                                command=lambda: self.connect4())
+        multiplayer_B3 = Button(self.window, text="Back", fg='deep sky blue', width=500, font='summer', bd=5,
+                                command=lambda: (self.window.destroy(), Main_menu()))
+        multiplayer_B1.pack(side='top', pady=15)
+        multiplayer_B2.pack(side='top', pady=15)
+        multiplayer_B3.pack(side='top', pady=15)
+        self.window.mainloop()
+
+    def menu(self):
+        self.window.title("main menu ")
+
+        head = Button(self.window, text="---Welcome to the main menu---", fg='deep sky blue', width=500, pady=20,
+                      font='summer',
+                      bd=5)
+        B1 = Button(self.window, text="solo", fg='deep sky blue', width=500, pady=15, font='summer', bd=5,
+                    command=lambda: self.solo())
+        B2 = Button(self.window, text="multiplayer", fg='deep sky blue', width=500, pady=15, font='summer', bd=5,
+                    command=lambda: self.multiplayer())
+        B3 = Button(self.window, text="Exit", fg='deep sky blue', width=500, pady=15, font='summer', bd=5,
+                    command=self.window.quit)
+
+        head.pack(side='top', pady=15)
+        B1.pack(side='top', pady=15)
+        B2.pack(side='top', pady=15)
+        B3.pack(side='top', pady=15)
+        self.window.mainloop()
+
+    def create(self):
+        self.window = Tk()
         width = 600
         height = 600
         screenwidth = self.window.winfo_screenwidth()
@@ -119,36 +95,7 @@ class TicTacToeGame:
         self.window.geometry(alignstr)
         self.window.resizable(width=False, height=False)
 
-        for row in range(3):
-            self.window.rowconfigure(row, weight=1, minsize=50)
-            self.window.columnconfigure(row, weight=1, minsize=75)
-            for col in range(3):
-                button = tk.Button(self.window)
-                button["justify"] = "center"
-                button["text"] = "Button"
-                button.place(x=0, y=0, width=200, height=200)
-                button["command"] = self.button()
 
-                # self._cells[button] = (row, col)
-                button.grid(
-                    row=row,
-                    column=col,
-                    padx=5,
-                    pady=5,
-                    sticky="nsew"
-                )
-        return "Hello"
-
-    def difficulty_button(self):
-        return "Hello"
-
-    def leave_button(self):
-        return "Hello"
-
-    def button(self):
-        return "Hello"
-
-
-# Main Code (Temporary)
-game = TicTacToeGame()
-game.run()
+# Call main function
+if __name__ == '__main__':
+    Main_menu()
