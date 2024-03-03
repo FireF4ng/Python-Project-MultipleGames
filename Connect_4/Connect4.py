cf44ba0f562a355e4ed26476f8c1e3f032197ed4
FireFang <118684614+FireF4ng@users.noreply.github.com>
2024-03-03 20:47:01 +0100

    Connect4 start

--- Connect_4/Connect4.py
+++ Connect_4/Connect4.py
@@ -11,14 +11,15 @@ class Connect4:
         self.main_menu_instance = main_menu_instance
         # self.c4_game = tk.Tk()
         self.label = None
-        self.board = [[None for _ in range(7)] for _ in range(6)]
-        self.red = PhotoImage(file="red.png")
-        self.yellow = PhotoImage(file="yellow.png")
+        self.board = [[0 for _ in range(7)] for _ in range(6)]
+        # self.red = PhotoImage(file="red.png")
+        # self.yellow = PhotoImage(file="yellow.png")
         self.player = ''
         self.pc = ''
         self.turn = ''
         self.winner = ''
         self.coin_toss()
+        self.game()
         # self.main_menu()
 
     # def main_menu(self):
@@ -82,6 +83,79 @@ class Connect4:
     # def button(self, x, y):
     #     print("Button clicked at:", x, y)
 
+    def game(self):
+        while not self.check_winner() and not self.check_draw():
+            self.next_player()
+            self.show_board()
+            print("It's ", self.player, " turn")
+            x = int(input("Enter col you want to play: ")) - 1
+            for i in range(len(self.board) - 1, -1, -1):
+                if self.board[i][x] == 0:
+                    if self.player == 'red':
+                        self.board[i][x] = 1
+                    else:
+                        self.board[i][x] = 2
+                    break
+        if self.check_winner():
+            self.show_board()
+            print("Winner", self.player)
+        elif self.check_draw():
+            self.show_board()
+            print('Draw!')
+
+    def check_winner(self):
+        # Check rows
+        for row in self.board:
+            tmp = 0
+            for elem in range(len(row) - 1):
+                if row[elem] == row[elem + 1] != 0:
+                    tmp += 1
+            if tmp == 3:
+                return True
+
+        # Check cols
+        for row in range(len(self.board)):
+            tmp = 0
+            for col in range(len(self.board[0]) - 2):
+                if self.board[col][row] == self.board[col + 1][row] != 0:
+                    tmp += 1
+            if tmp == 3:
+                return True
+
+        # Check pos diagonals
+        for col in range(len(self.board[0]) - 3):
+            for row in range(len(self.board) - 3):
+                if (
+                        self.board[row][col] == self.board[row + 1][col + 1] == self.board[row + 2][col + 2] == self.board[row + 3][col + 3] != 0
+                ):
+                    return True
+
+
+        # Check neg diagonals
+        for col in range(len(self.board[0]) - 3):
+            for row in range(3, len(self.board)):
+                if (
+                        self.board[row][col] == self.board[row - 1][col + 1] == self.board[row - 2][col + 2] == self.board[row - 3][col + 3] != 0
+                ):
+                    return True
+
+        # No Winners
+        return False
+
+    def check_draw(self):
+        """Function that checks for a draw"""
+        for i in self.board:
+            if 0 not in i:
+                return True
+        return False
+
+    def show_board(self):
+        for i in self.board:
+            print(i)
+        print(" ", end="")
+        for i in range(1, len(self.board[0]) + 1):
+            print(i, end='  ')
+        print('')
 
     def coin_toss(self):
         """Function that determines what player goes first as X and what player gets 0"""
@@ -114,6 +188,7 @@ class Connect4:
             self.turn = 'player'
         else:
             self.turn = 'pc'
+
 
 if __name__ == '__main__':
     Connect4(None, 0)
